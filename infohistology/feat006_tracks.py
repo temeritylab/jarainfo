92c825459453d4b94baadb38a8da4868c5bcf675
Jenny Mohn <jlmohn@ucdavis.edu>
2022-04-15 14:39:47 -0700

    added feat006 tracks file

---
+++ infohistology/feat006_tracks.py
@@ -0,0 +1,25 @@
+subject = 'feat006'
+tracks = [
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_B5_R',
+     'recordingTrack':'caudomedialDiI', 'shank':1, 'atlasZ':192}, #p1_B1 for tip, p1_B5 for ctx
+
+    {'subject':subject, 'brainArea':'AC_right', 'histImage':'p1_C3_R', #p1_B5 for tip, p1_C3 for ctx
+     'recordingTrack':'middlemedialDiD', 'shank':1, 'atlasZ':208},
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_A3_L',
+     'recordingTrack':'caudomedialDiI', 'shank':1, 'atlasZ':170}, #p1_A1 for tip, p1_A3 for ctx
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_A6_L',
+     'recordingTrack':'middlelateralDiI', 'shank':1, 'atlasZ':188}, #p1_A3 for tip, p1_A6 for ctx
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_B6_L',
+     'recordingTrack':'anteromedialDiI', 'shank':1, 'atlasZ':216}, #p1_B2 for tip, p1_B6 for ctx
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_B5_L',
+     'recordingTrack':'anterolateralDiD', 'shank':1, 'atlasZ':212}, #p1_B1 for tip, p1_B5 for ctx
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_B3_L',
+     'recordingTrack':'middlemedialDiD', 'shank':1, 'atlasZ':200}, #p1_B1 for tip, p1_B3-6 for ctx
+
+    {'subject':subject, 'brainArea':'AC_left', 'histImage':'p1_A3_L',
+     'recordingTrack':'caudolateralDiD', 'shank':1, 'atlasZ':170} #p1_A1 for tip, p1_A3 for ctx]
